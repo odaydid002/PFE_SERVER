93993348deed75a542a7c20bf145454d4a834005
Oudai Oulhadj <88271352+odaydid002@users.noreply.github.com>
2025-03-27 16:25:39 +0100

    Update emailController.js

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -50,7 +50,7 @@ exports.sendLoginLink = async (req, res) => {
     users.set(token, { email, expires: Date.now() + 30 * 60 * 1000 });
 
     // Secure login link
-    const loginLink = `http://127.0.0.1:5500/home/signup.html?token=${token}`;
+    const loginLink = `http://localhost:5000/signup?token=${token}`;
 
     // Configure Nodemailer
     const transporter = nodemailer.createTransport({
